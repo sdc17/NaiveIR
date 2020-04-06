0cc98ca4cfaf12d29c573f5608d4424d2d4bcfd2
sdc17 <shidachuan17@gmail.com>
2020-04-06 08:22:24 +0800

    cmt

---
+++ temp.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+# a = np.array([1, 2, 6, 3, 4, 9, 5, 8])
+# print(np.argsort(a)[:5])
+# print(a)
+
+# input_vis = cv2.imread('./seg_beach/SegmentationClassVisualization/beach.jpg')
+# plt.imshow(input_vis[:, :, ::-1])
+# plt.show()
+
+# test = np.array([[12,4,7,0],[3,7,45,81]])
+
+# # 对test进行转置操作
+# x = test.transpose()
+
+# print(x)
+# print(test)
+
+# a = np.array([1, 2, 3, 0])
+# # b = np.array([6, 7, 8])
+# # x = ((a + b)/2).astype(np.uint8)
+# # print(x)
+# x = np.sort(a)
+# print(x)
+# print(a)
+
+a = np.array([1, 2, 3, 0])
+print(a + 1)
